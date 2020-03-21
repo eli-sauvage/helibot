92602bee665d6b9e4b40bd6279dd345546e93ab9
elicolh <36312455+elicolh@users.noreply.github.com>
2020-03-21 13:48:50 +0100

    Create poll.js

---
+++ bot/poll.js
@@ -0,0 +1,87 @@
+let reacts = [
+  "1️⃣",
+  "2️⃣",
+  "3️⃣",
+  "4️⃣",
+  "5️⃣",
+  "6️⃣",
+  "7️⃣",
+  "8️⃣",
+  "9️⃣",
+  "🔟",
+  "🇦",
+  "🇧",
+  "🇨",
+  "🇩",
+  "🇪",
+  "🇫",
+  "🇬",
+  "🇭",
+  "🇮",
+  "🇯",
+  "🇰",
+  "🇱",
+  "🇲",
+  "🇳",
+  "🇴"
+];
+var channel;
+var discord;
+var testDouble = function(arr){
+  var enDouble = []
+  	arr.forEach(e=>{
+      if(arr.filter(item => item == e).length > 1){
+        if(!enDouble.includes(e))
+          enDouble.push(e)
+      }
+	})
+  return enDouble
+}
+class exp {
+  init(c, d) {
+    channel = c;
+    console.log(reacts.length);
+    discord = d;
+  }
+  newPoll(q, r, m) {
+        console.log("poll");
+    var msg = new discord.RichEmbed()
+      .setColor("#0099ff")
+      .setTitle("Sondage")
+      .setURL("https://discordbotv2.glitch.me/poll")
+      .setAuthor("helibot","https://images-na.ssl-images-amazon.com/images/I/615Q1Ms%2Bb4L._SX425_.jpg")
+      .setDescription(q)
+      .setTimestamp()
+      .setFooter("https://discordbotv2.glitch.me/poll")
+    var i = 0;
+    r.forEach(e => {
+      msg.addField(e, reacts[i], true);
+      i++;
+    });
+    if(m)
+      channel.send("@everyone, " + q).then(e=>e.delete())
+    channel.send(msg).then(async function(m){
+      for(var i=0;i<r.length;i++){
+        await m.react(reacts[i])
+      }
+    }).catch();
+  }
+  newReact(react,user){
+    if(react.me || !react.users.array().map(e=>e.bot).includes(true))
+      return
+    var userArray = []
+    Array.from(react.message.reactions).forEach(e=>{
+      userArray = userArray.concat(e[1].users.array())
+    })
+    console.log(userArray.map(e=>e.username))
+    if(testDouble(userArray).length && testDouble(userArray).includes(user)){
+      var msg = "double vote : " 
+      testDouble(userArray).map(e=>e.username).forEach(e=>{
+        msg = msg.concat(e + " ")
+      })
+      //react.message.channel.send(msg)
+      //console.log("double=>" + testDouble(userArray).map(e=>e.username))
+    }
+  }
+}
+module.exports = new exp();
