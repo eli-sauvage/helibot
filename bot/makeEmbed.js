d964c6b1688c62475e2fe7f8177c32913b7e6fdc
elicolh <36312455+elicolh@users.noreply.github.com>
2020-03-22 01:13:01 +0100

    Update makeEmbed.js

--- bot/makeEmbed.js
+++ bot/makeEmbed.js
@@ -12,8 +12,8 @@ module.exports = function (members) {
       "Helibot",
       "https://images-na.ssl-images-amazon.com/images/I/615Q1Ms%2Bb4L._SX425_.jpg"
     )
-    .setTitle("https://murmuring-dawn-90139.herokuapp.com (pour tous les scores)")
-    .setURL("https://murmuring-dawn-90139.herokuapp.com")
+    .setTitle("site en cours")//https://murmuring-dawn-90139.herokuapp.com (pour tous les scores)")
+    .setURL("site en cours")//https://murmuring-dawn-90139.herokuapp.com")
     .setColor(0)
     .setDescription("scores des 15 premiers :")
     .addBlankField(true);
