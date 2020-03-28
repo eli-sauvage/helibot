99a1197855d7141cb4967e9586c0eafbf8d96dff
elicolh <36312455+elicolh@users.noreply.github.com>
2020-03-28 14:45:22 +0100

    npm cors module test

--- bot/express.js
+++ bot/express.js
@@ -3,6 +3,7 @@ const http = require("http");
 const testpl = require("./testpl");
 const shell = require("shelljs")
 var bodyParser = require("body-parser");
+const cors = require("cors")
 class exp {
   init(members, poll, appDirName) {
     const app = express();
@@ -14,24 +15,25 @@ class exp {
         extended: true
       })
     );
-    app.use(function (req, res, next) {
+    app.use(cors())
+//     app.use(function (req, res, next) {
 
-    // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8888');
+//     // Website you wish to allow to connect
+//     res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8888');
 
-    // Request methods you wish to allow
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+//     // Request methods you wish to allow
+//     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
 
-    // Request headers you wish to allow
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+//     // Request headers you wish to allow
+//     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
 
-    // Set to true if you need the website to include cookies in the requests sent
-    // to the API (e.g. in case you use sessions)
-    res.setHeader('Access-Control-Allow-Credentials', true);
+//     // Set to true if you need the website to include cookies in the requests sent
+//     // to the API (e.g. in case you use sessions)
+//     res.setHeader('Access-Control-Allow-Credentials', true);
 
-    // Pass to next layer of middleware
-    next();
-});
+//     // Pass to next layer of middleware
+//     next();
+// });
     app.get("/", function(request, response) {
       response.sendFile(appDirName + "/public/scores.html");
       console.log("req")
