afd6652d5abc5675644a45850b97c7e27f036a39
elicolh <36312455+elicolh@users.noreply.github.com>
2020-03-28 16:12:51 +0100

    Update main.js

--- main.js
+++ main.js
@@ -19,7 +19,7 @@ var up = function(client, guild,fs,fetch) {
 };
 
 client.on("ready", function() {
-  console.log("gjyi")
+  console.log("trh")
   guild = client.guilds.find(e => e.id == "532956456492728320");
   scoreChannel = guild.channels
     .array()
