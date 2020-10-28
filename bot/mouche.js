da523242cc62f2cc19c788a266bdf993d3fb4adb
elicolh <36312455+elicolh@users.noreply.github.com>
2020-10-29 00:29:15 +0100

    Update mouche.js

--- bot/mouche.js
+++ bot/mouche.js
@@ -3,4 +3,4 @@ let client = new discord.Client()
 client.on("guildMemberRemove",(memb)=>{
     memb.guild.channels.cache.find(e=>e.name=="général").send("<@" + memb.id + "> a pris la mouche (a quitté le Molec)")
 })
-client.login("NTQ0OTUyNzYxODkzMjU3MjMx.XGMUzw.hGP0lr47BmIwMhdFqSOgC-qGRm4")
+client.login(require("./../token"))
