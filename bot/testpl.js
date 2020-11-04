47222e21c999deb4b544c331659d25ee99a05205
elicolh <36312455+elicolh@users.noreply.github.com>
2020-11-04 18:20:53 +0100

    Update testpl.js

--- bot/testpl.js
+++ bot/testpl.js
@@ -1,16 +1,16 @@
 module.exports = function(memb) {
-  if (memb.user.voiceChannel == undefined) return false;
+  if (memb.user.voice.channel == undefined) return false;
   if (
-    memb.user.voiceChannel.members.cache.array().length >= 2 && //au moins 2 personnes ds channel
+    memb.user.voice.channel.members.array().length >= 2 && //au moins 2 personnes ds channel
     !memb.user.deaf &&
     !memb.user.mute &&
     !(
-      (memb.user.voiceChannel.members.cache
+      (memb.user.voice.channel.members
         .array()
         .map(e => e.user.bot)
         .includes(true) &&
-        memb.user.voiceChannel.members.cache.array().length == 2) ||
-      memb.user.voiceChannel.members.cache.array().length == 1
+        memb.user.voice.channel.members.array().length == 2) ||
+      memb.user.voice.channel.members.array().length == 1
     ) //pas tout seul avec le bot
   )
     return true;
