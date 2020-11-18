5ca92d780ee6ce82ecbd9d71a84fc947f8efc87b
elicolh <36312455+elicolh@users.noreply.github.com>
2020-11-18 13:44:34 +0100

    Update mouche.js

--- bot/mouche.js
+++ bot/mouche.js
@@ -1,6 +1,6 @@
 let discord = require("discord.js")
 let client = new discord.Client()
 client.on("guildMemberRemove",(memb)=>{
-    memb.guild.channels.cache.find(e=>e.name=="général").send(memb.nickname + " a pris la mouche (a quitté " + memb.guild.name + ")")
+    memb.guild.channels.cache.find(e=>e.name=="général").send(memb.user.username + " a pris la mouche (a quitté " + memb.guild.name + ")")
 })
 client.login(require("./../token"))
