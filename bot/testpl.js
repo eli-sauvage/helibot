7298739ba7c4995ba2249bc8f2b7e9dc2493242c
elicolh <36312455+elicolh@users.noreply.github.com>
2021-01-08 19:39:05 +0100

    Update testpl.js

--- bot/testpl.js
+++ bot/testpl.js
@@ -2,8 +2,8 @@ module.exports = function(memb, guild) {
   if (memb.user.voice.channel == undefined) return false;
   if (
     memb.user.voice.channel.members.array().length >= 2 && //au moins 2 personnes ds channel
-    !memb.user.deaf &&
-    !memb.user.mute &&
+    !memb.user.voice.deaf &&
+    !memb.user.voice.mute &&
     guild.afkChannel.id != memb.user.voice.channelID &&
     !(
       (memb.user.voice.channel.members
