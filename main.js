fe4282639e07b90039e4f72d3317bab28ad2c10e
elicolh <36312455+elicolh@users.noreply.github.com>
2021-03-18 20:37:19 +0100

    clean main / updated TODO

--- main.js
+++ main.js
@@ -1,6 +1,14 @@
 const discord = require("discord.js");
-// require("./bot/graphique/chart")
 var client = new discord.Client({ws:{intents:discord.Intents.ALL}});
+
+var promises = [
+    require("./bot/sqlQueries").init(),
+    new Promise((res, rej)=>{
+        client.on("ready", res)
+    })
+]
+
+Promise.all(promises).then(params=>onReady(params[0]))
 
 async function onReady(query){
     console.log("successfully connected to discord.com and to the database")
@@ -8,33 +16,5 @@ async function onReady(query){
     new (require("./bot/points"))(client)
     new (require("./bot/messageScores"))(client)
 }
-var promises = [
-    require("./bot/sqlQueries").init(),
-    new Promise((res, rej)=>{
-        client.on("ready", res)
-    })
-]
-Promise.all(promises).then(params=>onReady(params[0]))
 
-// client.on("ready", async ()=>{
-    // console.log("bot pret");
-    // new (require("./bot/points"))(client)
-    // new (require("./bot/messageScores"))(client)
-    // let query = await require("./bot/sqlQueries").init()
-    // query.fillNULL()
-    // let sql = new require("./bot/sqlconnector")("helibot")
-    
-    // console.log(client.guilds.cache.find(e=>e.id == '532956456492728320').members.cache.find(e=>e.nickname == "Elic de 3").id)
-    // console.log(client.guilds.cache.find(e=>e.id == '532956456492728320').members.cache.find(e=>e.nickname == "Elic de 3").user.id)
-
-
-    
-    /////
-    // dev(query)
-    // dev()
-    // setInterval(()=>dev(query), 1000)
-// })
-async function dev(q){
-
-}
 client.login(require("./token").discord)
