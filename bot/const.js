35e53c7b0652c7f4ddf843aa6aee4fb84fb406e6
root <root@elicolh.example.com>
2021-08-01 21:40:51 +0000

    refactor to TypeScript

--- bot/const.js
+++
@@ -1,7 +0,0 @@
-module.exports = true?{//molec
-    guildId:'532956456492728320',//molec
-    scoreChannelId:"672858838810230799"
-}:{
-    guildId:'544953131205918720',//projet rework
-    scoreChannelId:"710990681103138868"
-}
