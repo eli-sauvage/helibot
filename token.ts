54f95d763a1595439edd82ace5ebebb7c8afbdac
root <root@elicolh.example.com>
2021-08-01 21:47:53 +0000

    .

--- token.ts
+++
@@ -1,3 +0,0 @@
-export let discord = "NTQ0OTUyNzYxODkzMjU3MjMx.XGMUzw.uTgc8jEk0cIvhwaPi8DWj6Jywcc"
-export let sql = '^BEouWy7RN36$6bWrApxB^u'
-export let lol = "RGAPI-2a201077-06b2-4ec9-8e1c-30158c4635f2"
