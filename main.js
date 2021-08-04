1e4910dce47c8251d8b735817d6a16ce37f9c9c5
elicolh <36312455+elicolh@users.noreply.github.com>
2021-08-04 17:58:42 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 const { ShardingManager } = require('discord.js');
 
-const manager = new ShardingManager('./index.js', { token: require("./token")) });
+const manager = new ShardingManager('./index.js', { token: require("./token") });
 
 manager.on('shardCreate', shard => console.log(`Launched shard ${shard.id}`));
 
